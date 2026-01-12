0a38f43b388b21237aef7bdd77cdcf584d76b434
Santiago Zapata <dev.santizapata@gmail.com>
2026-01-11 20:56:42 -0500

    Implement new function for json-server get

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const registerBtn = document.querySelector("#register");
 const tableBody = document.querySelector("#table-body");
 const app_url = 'http://localhost:3000/';
 const searchBtn = document.querySelector("#search-btn");
-getElement("users")
+getElement(app_url + "users")
 
 registerBtn.addEventListener("click",function() {
     postData("users")
@@ -20,45 +20,25 @@ searchBtn.addEventListener("click", function() {
 })
 
 //Functions
-function getElement(data) {
-    fetch(app_url + data)
-        .then(res => res.json())
-        .then(data => {
-            let elements = "";
-            data.forEach(user => {
-                elements += `
-                <tr>
-                    <td>${user.id}</td>
-                    <td>${user.name}</td>
-                    <td>${user.lastname}</td>
-                    <td><button class="delete" onclick="deleteBtn('${user.id}')">Delete</button></td>
-                </tr>
-                `
-            });
-            tableBody.innerHTML = elements;
-        });
-}
-
-function postData(data) {
-    fetch(app_url + data, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name: inputName.value,
-            lastname: inputLastname.value
-        })
-    })
-    .then(res => res.json())
-    .then(data => data);
-}
-
-function deleteData(data) {
-    fetch(app_url + data, {
-        method: "DELETE"
-    })
-    .then(data => data);
+async function getElement(url) {
+    let items = "";
+  try {
+    const res = await fetch(url);
+    const users = await res.json();
+    for (user of users) {
+        items += `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.lastname}</td>
+            <td><button class="delete" onclick="deleteBtn('${user.id}')">Delete</button></td>
+        </tr>
+        `;
+    }
+    tableBody.innerHTML = items;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 function deleteBtn(id) {
